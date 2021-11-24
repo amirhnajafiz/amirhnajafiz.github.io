704743f0e471c58153c2901c72283b29c79aa2bb
amirhnajafiz <najafizadeh21@gmail.com>
2021-11-24 12:43:10 +0330

    JS API added

--- js/app.js
+++ js/app.js
@@ -1,28 +1,11 @@
 // Importing the data
 import data from "../../assets/data.js";
+import display_bio from "./api/bio.js";
 
 console.log("200 OK\n" + JSON.stringify(data));
-
-function display_bio() {
-    let temp = document.createElement("div");
-
-    Object.keys(data).forEach(key => {
-        let inner = document.createElement("div");
-        inner.classList.add("p-3");
-        inner.classList.add("my-2");
-        inner.classList.add("bg-dark");
-        inner.classList.add("rounded");
-        inner.classList.add("text-light");
-        inner.innerText = key + ": " + data[key];
-        temp.appendChild(inner);
-    });
-
-    document.getElementById("data").appendChild(temp);
-}
-
 
 let url = document.URL.split("/");
 
 if (url[url.length - 1] == "projects.html") {
-    display_bio();
+    display_bio(data);
 }
