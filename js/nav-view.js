b9f013f18048d8fcef97ac455e44b091210908c7
amirhnajafiz <najafizadeh21@gmail.com>
2022-08-01 19:40:42 +0430

    add: animation

--- js/nav-view.js
+++ js/nav-view.js
@@ -1,8 +1,23 @@
 const components = ["home-component", "bio-component", "tech-component"];
+const buttons = ["bt-1", "bt-2", "bt-3"]
 const disappear = "disappear";
 const appear = "appear"
+const select_btn = "selected-btn"
 
-function show(selected) {
+function updateBtn(bid) {
+    buttons.forEach((id) => {
+        const obj = document.getElementById(id)
+        if (id === bid) {
+            obj.classList.add(select_btn)
+        } else {
+            obj.classList.remove(select_btn)
+        }
+    })
+}
+
+function show(selected, btn) {
+    updateBtn(btn);
+
     components.forEach((id) => {
         const obj = document.getElementById(id)
         if (id === selected) {
@@ -12,5 +27,5 @@ function show(selected) {
             obj.classList.remove(appear)
             obj.classList.add(disappear)
         }
-    })
+    });
 }
