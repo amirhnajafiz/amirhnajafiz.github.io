b9f3d37a82dce23463fb0bb9e5770e13ab6234e1
amirhnajafiz <najafizadeh21@gmail.com>
2022-08-02 09:46:40 +0430

    update: router view

--- js/nav-view.js
+++ js/nav-view.js
@@ -1,4 +1,17 @@
-const components = ["home-component", "bio-component", "tech-component"];
+const components = [
+    {
+        id: "home-component",
+        color: "rgba(2, 193, 248, 0.2)"
+    },
+    {
+        id: "bio-component",
+        color: "rgba(11, 103, 0, 0.2)"
+    },
+    {
+        id: "tech-component",
+        color: "rgba(255, 212, 45, 0.2)"
+    }
+];
 const buttons = ["bt-1", "bt-2", "bt-3"]
 const disappear = "disappear";
 const appear = "appear"
@@ -18,9 +31,12 @@ function updateBtn(bid) {
 function show(selected, btn) {
     updateBtn(btn);
 
-    components.forEach((id) => {
-        const obj = document.getElementById(id)
-        if (id === selected) {
+    const v = document.getElementsByClassName("context-body-box").item(0)
+
+    components.forEach((c) => {
+        const obj = document.getElementById(c.id)
+        if (c.id === selected) {
+            v.style.background = c.color
             obj.classList.remove(disappear)
             obj.classList.add(appear)
         } else {
