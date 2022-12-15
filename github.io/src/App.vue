3850c94c9fade086d2779cfa1e61b8cde103715c
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-15 18:55:46 +0330

    fix: home

--- github.io/src/App.vue
+++ github.io/src/App.vue
@@ -1,69 +1,14 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
   <div>
-    <header>
-      <div class="wrapper">
-        <nav>
-          <RouterLink to="/">Home</RouterLink>
-          <RouterLink to="/honors">Honors</RouterLink>
-          <RouterLink to="/projects">Projects</RouterLink>
-        </nav>
-      </div>
-    </header>
+    <Navbar />
     <RouterView />
   </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
---
+++ github.io/src/components/Navbar.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <div class="header">
+    <div class="wrapper">
+      <div class="nav">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/honors">Honors</RouterLink>
+        <RouterLink to="/projects">Projects</RouterLink>
+        <a href="https://www.github.com">Github</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
+
+<style scoped>
+.header {
+  max-height: 100vh;
+
+  padding: 20px 0;
+
+  width: 100%;
+  margin-bottom: 40px;
+
+  background-color: #282828;
+  color: #f8f8f8;
+
+  position: sticky;
+  top: 0;
+}
+
+.nav {
+  width: 100%;
+
+  font-size: 14px;
+}
+
+.nav a {
+  display: inline-block;
+
+  padding: 0 3rem;
+
+  outline: none;
+  color: inherit;
+}
+</style>
