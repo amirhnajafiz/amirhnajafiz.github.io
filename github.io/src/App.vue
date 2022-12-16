93d2311ce830e7dfdea68b7fb6dc426fd1d85812
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-16 20:07:54 +0330

    add: footer

--- github.io/src/App.vue
+++ github.io/src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
   <div>
     <Navbar />
     <RouterView />
+    <Footer />
   </div>
 </template>
 
