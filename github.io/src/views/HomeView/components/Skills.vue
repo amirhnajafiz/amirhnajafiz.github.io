1fbcb9bf453fa6bfda102c36286f441a0868f2f0
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-20 13:11:24 +0330

    add: skills

--- github.io/src/views/HomeView/components/Skills.vue
+++ github.io/src/views/HomeView/components/Skills.vue
@@ -1,11 +1,61 @@
 <template>
   <div class="container">
+    <div
+        class="box"
+        v-for="topic in topics"
+    >
+      <div class="header">
+        {{ topic.title }}
+      </div>
+      <ul class="item-holder" style="list-style-type: none;">
+        <li
+            class="item"
+            v-for="skill in topic.skills"
+        >
+          {{ skill }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Skills"
+  name: "Skills",
+  data() {
+    return {
+      topics: [
+        {
+          title: "DevOps",
+          skills: [
+              "no",
+              "noting"
+          ]
+        },
+        {
+          title: "SRE",
+          skills: [
+              "hi",
+              "something"
+          ]
+        },
+        {
+          title: "Backend",
+          skills: [
+              "hello"
+          ]
+        },
+        {
+          title: "Software",
+          skills: [
+              "dp",
+              "algo",
+              "ds"
+          ]
+        }
+      ]
+    }
+  }
 }
 </script>
 
@@ -13,5 +63,37 @@ export default {
 .container {
   width: 100%;
   background-color: #00bd7e;
+
+  padding: 50px;
+  margin-bottom: 80px;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+
+  color: #2c3e50;
+}
+
+.box {
+  border: 1px solid #f8f8f8;
+  border-radius: 5px;
+}
+
+.header {
+  font-size: 2rem;
+
+  padding: 40px 60px;
+
+  background-color: #f8f8f8;
+
+  border-bottom: 1px solid #f8f8f8;
+}
+
+.item-holder {
+  padding: 40px 60px;
+}
+
+.item {
+  margin-bottom: 5px;
 }
 </style>
