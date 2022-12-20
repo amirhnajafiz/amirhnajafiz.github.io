0127967bac2b1aea39f06e26088df8801770ab29
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-20 12:07:37 +0330

    add: icons

--- github.io/src/components/Footer.vue
+++ github.io/src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    Footer
+  <div class="footer">
+    create and design by amirhossein-najafizadeh
   </div>
 </template>
 
@@ -11,5 +11,13 @@ export default {
 </script>
 
 <style scoped>
+.footer {
+  text-align: center;
 
+  padding: 10px 0;
+
+  color: #bdbdbd;
+  font-size: 0.8em;
+  border-top: 1px solid #dcdcdc;
+}
 </style>
