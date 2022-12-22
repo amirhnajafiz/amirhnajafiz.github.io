f36240504c9711c63e9c7cdce9f75b7a06d484bc
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-22 13:41:50 +0330

    add: project

---
+++ github.io/src/views/ProjectsView/component/Project.vue
@@ -0,0 +1,47 @@
+<script setup>
+defineProps({
+  title: {
+    type: String
+  },
+  description: {
+    type: String
+  },
+  link: {
+    type: String
+  },
+  image: {
+    type: String
+  },
+  color: {
+    type: String
+  },
+  background_color: {
+    type: String
+  }
+})
+</script>
+
+<template>
+  <div :style="'color: '+color+'; background-color:'+background_color">
+    <h3>
+      {{ title }}
+    </h3>
+    <img :src="image" alt="image" />
+    <p>
+      {{ description }}
+    </p>
+    <a :href="image" target="_blank">
+      View Project
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Project"
+}
+</script>
+
+<style scoped>
+
+</style>
