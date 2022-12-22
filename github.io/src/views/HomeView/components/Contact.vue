4acd54a42ff7072b40e8fb5f464b55c478184011
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-22 12:38:15 +0330

    init: contact

---
+++ github.io/src/views/HomeView/components/Contact.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="holder">
+    <div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Contact"
+}
+</script>
+
+<style scoped>
+.holder {
+  display: grid;
+  grid-template-columns: 40% 60%;
+
+  margin-bottom: 80px;
+}
+</style>
