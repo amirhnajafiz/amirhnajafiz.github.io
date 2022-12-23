88ace53fad165de2f6671fd8e97de9c091b3ba51
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-23 22:28:02 +0330

    add: stories

--- github.io/src/views/StoryView/components/Story.vue
+++ github.io/src/views/StoryView/components/Story.vue
@@ -1,5 +1,29 @@
-<template>
+<script setup>
+defineProps({
+  title: {
+    type: String
+  },
+  description: {
+    type: String
+  },
+  link: {
+    type: String
+  }
+})
+</script>
 
+<template>
+  <div>
+    <h3>
+      {{ title }}
+    </h3>
+    <p>
+      {{ description }}
+    </p>
+    <small>
+      {{ link }}
+    </small>
+  </div>
 </template>
 
 <script>
--- github.io/src/views/StoryView/index.vue
+++ github.io/src/views/StoryView/index.vue
@@ -1,10 +1,24 @@
 <template>
-
+  <div>
+    <div class="headline">
+      Stories
+    </div>
+    <div>
+      <Story
+        v-for="story in stories"
+        :title="story.title"
+        :description="story.description"
+        :link="story.link"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
+import Story from "./components/Story";
 export default {
   name: "StoryView",
+  components: {Story},
   data() {
     return {
       stories: [
@@ -35,5 +49,8 @@ export default {
 </script>
 
 <style scoped>
-
+.headline {
+  font-size: 5em;
+  padding: 50px 200px;
+}
 </style>
