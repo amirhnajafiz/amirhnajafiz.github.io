003857d4713877225bca5f31d5973a5923cbc7a9
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-23 22:25:27 +0330

    init: story view

---
+++ github.io/src/views/StoryView/components/Story.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "Story"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ github.io/src/views/StoryView/index.vue
@@ -0,0 +1,39 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "StoryView",
+  data() {
+    return {
+      stories: [
+        {
+          title: "title",
+          description: "desc",
+          link: "link"
+        },
+        {
+          title: "title",
+          description: "desc",
+          link: "link"
+        },
+        {
+          title: "title",
+          description: "desc",
+          link: "link"
+        },
+        {
+          title: "title",
+          description: "desc",
+          link: "link"
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
