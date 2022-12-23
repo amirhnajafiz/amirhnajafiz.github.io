aa426d55fbde2fd0610ec530d69ae3932265eb15
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-23 16:21:08 +0330

    update: project styles

--- github.io/src/views/ProjectsView/component/Project.vue
+++ github.io/src/views/ProjectsView/component/Project.vue
@@ -30,14 +30,19 @@ defineProps({
       {{ description }}
     </p>
     <div class="row">
-      <div>
-        {{ language }}
+      <div class="inner-row">
+        <div>
+          {{ language }}
+        </div>
+        <div
+            class="circle"
+            :style="'background-color:' + color"
+        ></div>
       </div>
-      <div
-          class="circle"
-          :style="'background-color:' + color"
-      ></div>
-      <a  class="link" :href="link" target="_blank">
+      <a
+          class="link"
+          :href="link"
+          target="_blank">
         See project
       </a>
     </div>
@@ -51,14 +56,34 @@ export default {
 </script>
 
 <style scoped>
+.wrapper {
+  width: 100%;
+}
+
 .row {
   font-size: 0.7em;
+
+  display: flex;
+  justify-content: space-between;
+}
+
+.inner-row {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+.circle {
+  width: 16px;
+  height: 16px;
+
+  margin-left: 5px;
+
+  border-radius: 50%;
 }
 
 .link {
-
   text-decoration-line: none;
-
   color: inherit;
 }
 </style>
