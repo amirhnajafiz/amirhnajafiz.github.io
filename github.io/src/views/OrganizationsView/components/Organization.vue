1a6850edbac4f72303434680bb1b8b06059732d8
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-23 22:14:08 +0330

    add: organization view

---
+++ github.io/src/views/OrganizationsView/components/Organization.vue
@@ -0,0 +1,45 @@
+<script setup>
+defineProps({
+  title: {
+    type: String
+  },
+  image: {
+    type: String
+  },
+  address: {
+    type: String
+  },
+  description: {
+    type: String
+  }
+})
+</script>
+
+<template>
+  <div class="wrapper">
+    <div>
+      {{ image }}
+    </div>
+    <div>
+      <h3>
+        {{ title }}
+      </h3>
+      <p>
+        {{ description }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Organization"
+}
+</script>
+
+<style scoped>
+.wrapper {
+  display: grid;
+  grid-template-columns: 30% 70%;
+}
+</style>
--- github.io/src/views/OrganizationsView/index.vue
+++ github.io/src/views/OrganizationsView/index.vue
@@ -1,10 +1,21 @@
 <template>
-
+  <div>
+    <Organization
+      v-for="organization in organizations"
+      :title="organization.title"
+      :address="organization.address"
+      :image="organization.image"
+      :description="organization.description"
+    />
+  </div>
 </template>
 
 <script>
+import Organization from "./components/Organization.vue";
+
 export default {
   name: "OrganizationsView",
+  components: {Organization},
   data() {
     return {
       organizations: [
