2efb8a5292308b4084044171a9ed9aab0add566a
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-24 21:58:25 +0330

    update: slideshow methods

--- github.io/src/views/HomeView/components/Slideshow.vue
+++ github.io/src/views/HomeView/components/Slideshow.vue
@@ -9,7 +9,42 @@
 
 <script>
 export default {
-  name: "Slideshow"
+  name: "Slideshow",
+
+  data() {
+    return {
+      images: [
+        "https://cdn.pixabay.com/photo/2015/12/12/15/24/amsterdam-1089646_1280.jpg",
+        "https://cdn.pixabay.com/photo/2016/02/17/23/03/usa-1206240_1280.jpg",
+        "https://cdn.pixabay.com/photo/2015/05/15/14/27/eiffel-tower-768501_1280.jpg",
+        "https://cdn.pixabay.com/photo/2016/12/04/19/30/berlin-cathedral-1882397_1280.jpg"
+      ],
+      timer: null,
+      currentIndex: 0
+    }
+  },
+
+  mounted: function() {
+    this.startSlide();
+  },
+
+  methods: {
+    startSlide: function() {
+      this.timer = setInterval(this.next, 4000);
+    },
+    next: function() {
+      this.currentIndex += 1;
+    },
+    prev: function() {
+      this.currentIndex -= 1;
+    }
+  },
+
+  computed: {
+    currentImg: function() {
+      return this.images[Math.abs(this.currentIndex) % this.images.length];
+    }
+  }
 }
 </script>
 
