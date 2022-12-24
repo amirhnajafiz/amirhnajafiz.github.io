afb17d9b1aa27383d58de4a3446b410b10f58f8b
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-24 23:24:43 +0330

    update: intrests

--- github.io/src/views/HomeView/components/Interests.vue
+++ github.io/src/views/HomeView/components/Interests.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="wrapper">
     <div v-for="interest in interests">
-      <h1>
+      <h2 style="margin-bottom: 10px;">
         {{ interest.title }}
-      </h1>
-      <p>
+      </h2>
+      <p style="text-align: justify;">
         {{ interest.description }}
       </p>
     </div>
@@ -18,36 +18,20 @@ export default {
     return {
       interests: [
         {
-          title: "title",
-          description: "des"
+          title: "Computer Networks",
+          description: "Everything about computers communications and system networks."
         },
         {
-          title: "title",
-          description: "des"
+          title: "DevOps",
+          description: "Automatic development operations."
         },
         {
-          title: "title",
-          description: "des"
+          title: "Internet of Things",
+          description: "All these real life smart things."
         },
         {
-          title: "title",
-          description: "des"
-        },
-        {
-          title: "title",
-          description: "des"
-        },
-        {
-          title: "title",
-          description: "des"
-        },
-        {
-          title: "title",
-          description: "des"
-        },
-        {
-          title: "title",
-          description: "des"
+          title: "Data Security",
+          description: "Encryption techniques and Hashing mechanisms."
         }
       ]
     }
@@ -57,14 +41,15 @@ export default {
 
 <style scoped>
 .wrapper {
-  background-color: #2c3e50;
-  color: #00bd7e;
+  background-color: #084887;
+  color: #f7f5fb;
 
   padding: 50px 100px;
   margin-bottom: 200px;
 
   display: grid;
-  grid-template-columns: auto auto auto auto;
+  grid-template-columns: 20% 20% 20% 20%;
   row-gap: 50px;
+  column-gap: 70px;
 }
 </style>
