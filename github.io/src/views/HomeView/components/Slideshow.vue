7215e0b8525877b018d42b6fad6cb0ec0f0d2211
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-24 22:53:06 +0330

    add: new show image

--- github.io/src/views/HomeView/components/Slideshow.vue
+++ github.io/src/views/HomeView/components/Slideshow.vue
@@ -1,5 +1,33 @@
 <template>
-  <div>
-    Fuck
+  <div class="holder">
+    <img src="assets/images/me.jpg" alt="show image" />
   </div>
 </template>
+
+<style scoped>
+.holder {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+img {
+  --g:#0000 ,#000 .5deg 90deg,#0000 91deg;
+  --m:
+      conic-gradient(from 45deg ,var(--g)) 20px 0  no-repeat,
+      conic-gradient(from 135deg,var(--g)) 0 20px  no-repeat,
+      conic-gradient(from 225deg,var(--g)) -20px 0 no-repeat,
+      conic-gradient(from 315deg,var(--g)) 0 -20px no-repeat;
+  -webkit-mask: var(--m);
+  mask: var(--m);
+  transition: .3s linear;
+  filter: grayscale(.5);
+  cursor: pointer;
+}
+
+img:hover {
+  -webkit-mask-position: 0 0;
+  mask-position: 0 0;
+  filter: grayscale(0);
+}
+</style>
