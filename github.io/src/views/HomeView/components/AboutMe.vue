6392a6383e8628bc8ab0ad539cdb03f318209448
amirhnajafiz <najafizadeh21@gmail.com>
2023-01-06 16:55:24 +0330

    update: aboutme

--- github.io/src/views/HomeView/components/AboutMe.vue
+++ github.io/src/views/HomeView/components/AboutMe.vue
@@ -11,7 +11,13 @@
       </div>
     </div>
     <div class="article">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      I'm a 21 years old Computer Engineer studying at the Amirkabir University of Tehran, Iran. My university
+      fields are Computer Networks and Software Engineering. At the moment I am doing researches in Software
+      Security, Fast Networks, and Virtualization.<br />
+      I also work as a full time Back-end Developer at Snapp! company. I start working in Snapp in December 2021.
+      But before that I used to teach computer basics and introduction to C and C++ at Iranian Cyber. I was also
+      mentor for Web Developing.<br />
+      I started my career as a Full Stack developer by learning Vue.js and PHP/Laravel.
     </div>
   </div>
 </template>
@@ -26,7 +32,6 @@ export default {
 .holder {
   display: grid;
   grid-template-columns: 40% 60%;
-
   margin-top: 120px;
   margin-bottom: 200px;
 }
@@ -38,39 +43,30 @@ export default {
 
 .box {
   position: relative;
-
   top: 50px;
   left: 100px;
-
   width: 200px;
   height: 200px;
-
   border-radius: 50%;
   background-color: #181818;
 }
 
 .text {
   position: relative;
-
   left: 50%;
-
   color: #f7f5fb;
   font-size: 2em;
 }
 
 .bold {
   font-size: 3em;
-
   color: #282828;
 }
 
 .article {
   background-color: #f9ab55;
-
   color: #282828;
-
   text-align: justify;
-
   padding: 80px 80px;
 }
 </style>
