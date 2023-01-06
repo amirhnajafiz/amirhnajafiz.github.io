c4217769f7c924a3d3c5f07eb13916e204373c58
amirhnajafiz <najafizadeh21@gmail.com>
2023-01-06 17:08:44 +0330

    update: honors

--- github.io/src/views/HonorsView/index.vue
+++ github.io/src/views/HonorsView/index.vue
@@ -22,19 +22,21 @@ export default {
     return {
       honors: [
         {
-          name: "Honor 1",
-          description: "Empty",
-          date: new Date()
+          name: "Secretary of the 13th edition of the Linux Festival",
+          description: "Secretary and organizer of the 13th Amirkabir University of Technology Linux Festival, April 2022.",
+          date:"2022 April"
         },
         {
-          name: "Honor 2",
-          description: "Empty",
-          date: new Date()
+          name: "2021 ACPC/ACM Staff",
+          description: "I was a part of the main staff in 2021 ICPC event in the Amirkabir University of Tehran, also known as ACPC.\n" +
+              "Had a great experience in holding such a big event with more than 200 teams participating. \n" +
+              "Special thanks to all of the other people who helped us to hold this event.",
+          date: "2021 November"
         },
         {
-          name: "Honor 3",
-          description: "Empty",
-          date: new Date()
+          name: "Top student of Maktab Sharif summer intern",
+          description: "I finished my summer intern at Maktab Sharif PHP/Laravel bootcamp as the top student.",
+          date: "2021 December"
         }
       ],
     }
