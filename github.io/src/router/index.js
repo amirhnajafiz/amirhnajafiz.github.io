a2b5fd5c192404ba93ba7b76c8aad170c12babd9
amirhnajafiz <najafizadeh21@gmail.com>
2023-07-06 20:19:07 +0330

    update: router

--- github.io/src/router/index.js
+++ github.io/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import HomeView from "../views/HomeView/index.vue"
+import BlogView from "../views/BlogView/index.vue"
+import ContactView from "../views/ContactView/index.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +11,16 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/blog',
+      name: 'blog',
+      component: BlogView
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: ContactView
     }
   ]
 })
