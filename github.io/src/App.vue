abdb4734d497c4ff4385ce45963f4fe35855b5d2
amirhnajafiz <najafizadeh21@gmail.com>
2023-07-06 20:16:54 +0330

    init: header

--- github.io/src/App.vue
+++ github.io/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
+import Header from "./components/Header.vue";
 import RoutesWithTransition from "./router/RoutesWithTransition.vue";
 </script>
 
 <template>
   <div>
+    <Header />
     <RoutesWithTransition />
   </div>
 </template>
---
+++ github.io/src/components/Header.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <div>
+    <RouterLink to="/">
+      Home
+    </RouterLink>
+    <RouterLink to="/blog">
+      Blog
+    </RouterLink>
+    <RouterLink to="/contact">
+      Contact
+    </RouterLink>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
