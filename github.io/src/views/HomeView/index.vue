8de1319c8f45b82ba9d353732b1ddfea374bf4e4
amirhnajafiz <najafizadeh21@gmail.com>
2023-07-07 10:45:16 +0330

    add: home page

--- github.io/src/views/HomeView/index.vue
+++ github.io/src/views/HomeView/index.vue
@@ -3,7 +3,30 @@
 </script>
 
 <template>
-  <div>
-    Yellow
+  <div class="holder partition">
+    <div>
+      <h1 style="margin-bottom: 20px;">
+        Welcome to my personal website.
+      </h1>
+      <p style="text-align: justify; line-height: 25px;">
+        My name is Amirhossein Najafizadeh. I'm 22 years old Web Designer, Backend Developer, Software and Network Engineer.
+        I study computer engineering at Amirkabir University of Tehran, Iran. And I work as a full-time midlevel sofware
+        engineer at Snappline!<br />
+        I've been working at Snapp! since winter of 2019 and I started my career as a Golang trainee. Before that I used to
+        work as a full-stack developer using PHP/Laravel for Backend and Vue.js for Frontend developing.<br />
+        My university major is computer networks, computers communication, cloud computing, and operating systems.
+        I am intrested in devops, infrastructure, network security, cloud computing, and software development.
+      </p>
+    </div>
+    <div style="text-align: center;">
+      <img style="border-radius: 4px;" src="https://avatars.githubusercontent.com/u/58415967?v=4" alt="self picture" width="300" />
+    </div>
   </div>
 </template>
+
+<style scoped>
+.partition {
+  display: grid;
+  grid-template-columns: 50% 50%;
+}
+</style>
