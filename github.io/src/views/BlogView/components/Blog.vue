535c32b85823e050b7e66403703052c60b7e03de
amirhnajafiz <najafizadeh21@gmail.com>
2023-07-07 11:11:25 +0330

    add: blogs page

---
+++ github.io/src/views/BlogView/components/Blog.vue
@@ -0,0 +1,35 @@
+<script setup>
+defineProps({
+    title: String,
+    description: String,
+    link: String,
+});
+</script>
+
+<template>
+    <div>
+        <div class="title">
+            {{ title }}
+        </div>
+        <p style="text-align: justify;">
+            {{ description }}
+        </p>
+        <div class="small">
+            visit blog at: <a v-bind:href="link">{{ link }}</a>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.title {
+    font-weight: bold;
+    font-size: 1.4em;
+    margin-bottom: 20px;
+}
+
+.small {
+    font-weight: lighter;
+    font-size: 0.8em;
+    margin-top: 20px;
+}
+</style>
--- github.io/src/views/BlogView/index.vue
+++ github.io/src/views/BlogView/index.vue
@@ -1,15 +1,84 @@
+<script setup>
+import Blog from './components/Blog.vue';
+</script>
+
 <template>
-  <div>
-    Blogs
+  <div class="holder">
+    <h2 style="margin-bottom: 20px;">
+      Blogs page
+    </h2>
+    <p>
+      Read my blogs and stories on Medium. These blogs are related to my work experince at Amirkabir University,
+      Snapp!, Snappline!, and any other place that I worked in the past.
+    </p>
+    <div class="blog-holder">
+      <Blog
+        class="blog-item"
+        v-for="item in blogs"
+        v-bind:title="item.title"
+        v-bind:description="item.context"
+        v-bind:link="item.link"
+        v-bind:key="item.id"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            blogs: [
+                {
+                  id: 1,
+                  title: "test 1",
+                  context: "context 1",
+                  link: "http://address"
+                },
+                {
+                  id: 2,
+                  title: "test 1",
+                  context: "context 1",
+                  link: "http://address"
+                },
+                {
+                  id: 3,
+                  title: "test 1",
+                  context: "context 1",
+                  link: "http://address"
+                },
+                {
+                  id: 3,
+                  title: "test 1",
+                  context: "context 1",
+                  link: "http://address"
+                },
+                {
+                  id: 3,
+                  title: "test 1",
+                  context: "context 1",
+                  link: "http://address"
+                }
+            ]
+        };
+    },
+    components: { Blog }
 }
 </script>
 
 <style>
+.blog-holder {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 30px;
+  margin-top: 30px;
+}
 
+.blog-item {
+  padding: 20px 25px;
+  border: 2px solid rgb(255, 197, 131);
+  border-radius: 4px;
+  width: 30%;
+}
 </style>
