00b5d9e9a2e2ae9a70da652575b97791eb2700e9
amirhnajafiz <najafizadeh21@gmail.com>
2023-07-07 11:43:49 +0330

    fix: build bugs

--- github.io/src/App.vue
+++ github.io/src/App.vue
@@ -1,12 +1,12 @@
 <script setup>
-import Header from "./components/Header.vue";
+import HeaderComponent from "./components/Header.vue";
 import Footer from "./components/Footer.vue"
 import RoutesWithTransition from "./router/RoutesWithTransition.vue";
 </script>
 
 <template>
   <div>
-    <Header class="navigation" />
+    <HeaderComponent class="navigation" />
     <RoutesWithTransition />
     <Footer class="footer" />
   </div>
--- github.io/src/components/Header.vue
+++ github.io/src/components/Header.vue
@@ -4,7 +4,7 @@ import { RouterLink } from 'vue-router'
 
 <template>
   <div>
-    <RouterLink class="navigation-item" v-bind:class="{'navigation-item-select' : this.$route.name === 'home'}" to="/">
+    <RouterLink class="navigation-item" v-bind:class="{'navigation-item-select' : routePath() === 'home'}" to="/">
       <div style="display: inline-block;">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
           <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z"/>
@@ -14,7 +14,7 @@ import { RouterLink } from 'vue-router'
         </span>
       </div>
     </RouterLink>
-    <RouterLink class="navigation-item" v-bind:class="{'navigation-item-select' : this.$route.name === 'blog'}" to="/blog">
+    <RouterLink class="navigation-item" v-bind:class="{'navigation-item-select' : routePath() === 'blog'}" to="/blog">
       <div style="display: inline-block;">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
           <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
@@ -27,3 +27,14 @@ import { RouterLink } from 'vue-router'
     </RouterLink>
   </div>
 </template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+  methods:{
+     routePath(){
+       return this.$route.name; 
+    },
+  }
+}
+</script>
