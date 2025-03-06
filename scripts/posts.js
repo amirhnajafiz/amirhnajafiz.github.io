bff459552f6576efdb42aac8f3521d5a34c376da
amirhnajafiz <najafizadeh21@gmail.com>
2025-03-05 23:33:53 -0500

    add: papers section

--- scripts/posts.js
+++ scripts/posts.js
@@ -1,29 +1,56 @@
 // all data of the webstie goes in this big object
 var posts = {
+  "papers": [
+    {
+      "content": "(A.H.Najafizadeh, Dr. S.A.Javadi) We propose a method to bypass low-priority tasks in Golang channel queues, improving upon the default FCFS behavior, and compare it with standard Golang channels.",
+      "url": "https://www.researchgate.net/publication/373271512_Jump_over_Golang_channels"
+    }
+  ],
   "blogs": [
     {
-      "content": "This is my first blog post. I hope you like it.",
-      "url": "my-first"
+      "content": "Having your Kubernetes over NFS",
+      "url": "https://medium.com/@amirhossein-najafizadeh/having-your-kubernetes-over-nfs-0510d5ed9b0b"
     },
     {
-      "content": "This is my second blog post. I hope you like it.",
-      "url": "my-first"
+      "content": "EMQX v5: Under Magnifier",
+      "url": "https://medium.com/@amirhossein-najafizadeh/emqx-v5-under-magnifier-ad4f6e0b8932"
+    },
+    {
+      "content": "Benchmarking NATS JetStream Cluster: Hypothesis Testing for Enhancements",
+      "url": "https://medium.com/@amirhossein-najafizadeh/benchmarking-nats-jetstream-cluster-hypothesis-testing-for-enhancements-4a500d11ce7b"
+    },
+    {
+      "content": "Minio shared objects operator",
+      "url": "https://medium.com/@amirhossein-najafizadeh/minio-shared-objects-operator-d4a718f40a0"
+    },
+    {
+      "content": "Kubernetes Cron-jobs (Python)",
+      "url": "https://medium.com/@amirhossein-najafizadeh/kubernetes-cron-jobs-python-6167135d5d8f"
     }
   ],
   "projects": [
     {
-      "content": "This is my first project. I hope you like it.",
-      "url": "my-first"
+      "content": "Penetration Testing as a Service, automated penetaion testing over cloud",
+      "url": "https://github.com/ptaas-tool"
     },
     {
-      "content": "This is my second project. I hope you like it.",
-      "url": "my-first"
+      "content": "Stallion; Fast message broker implemented with Golang.",
+      "url": "https://github.com/official-stallion/stallion"
     }
   ]
 };
 
 // use jQuery to place the data in the HTML
 $(document).ready(function() {
+    // loop through the papers posts and add them to the blog section
+      for (var i = 0; i < posts.papers.length; i++) {
+        var paper = posts.papers[i];
+        var blogHTML = '<div class="blog-post">';
+        blogHTML += '<span>' + paper.content + ' <a href="' + paper.url + '">' + paper.url + '</a></span>';
+        blogHTML += '</div>';
+        $('#papers').append(blogHTML);
+    }
+
     // loop through the blog posts and add them to the blog section
     for (var i = 0; i < posts.blogs.length; i++) {
         var blog = posts.blogs[i];
