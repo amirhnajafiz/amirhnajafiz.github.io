161e8c7fb7a0fc4d2dc945d03de7d4fc8dba1fbd
amirhnajafiz <najafizadeh21@gmail.com>
2025-03-07 19:10:07 -0500

    fix: init small windows bug

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,18 @@
-// window resize event
-$(window).resize(function() {
+// adjust the width of the columns based on the window size
+function adjustWidth() {
   if ($(window).width() < 768) {
     $('.col-md-6').removeClass('col-md-6').addClass('col-md-12');
     $('.col-md-12').addClass('mt-2');
-    $('.self-text').attr('style', 'text-align: center !important');
+    $('.self-text').attr('style', 'text-align: center !important; margin-buttom: 10px !important;');
   } else {
     $('.col-md-12').removeClass('mt-2');
     $('.col-md-12').removeClass('col-md-12').addClass('col-md-6');
     $('.self-text').removeAttr('style');
   }
-});
+}
 
-// window load event
-$(document).ready(function() {
+// add a button to scroll to the top of the page
+function createLiftButton() {
   $('<button/>', {
     text: 'Top',
     id: 'scrollToTop',
@@ -28,6 +28,17 @@ $(document).ready(function() {
   $('#scrollToTop').css('bottom', '10px');
   $('#scrollToTop').css('margin-left', '10px');
   $('#scrollToTop').css('display', 'none');
+}
+
+// window resize event
+$(window).resize(function() {
+  adjustWidth();
+});
+
+// window load event
+$(document).ready(function() {
+  adjustWidth();
+  createLiftButton();
 });
 
 // window scroll event
